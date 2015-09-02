199725da833a898e439635bf0f37345b22673b44
unknown <10571815@CS514-11.ad.uvu.edu>
2015-09-02 08:55:19 -0600

    Variables and Functions

---
+++ Unity1/Assets/NewBehaviourScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+/// <summary>
+/// 
+/// </summary>
+public class NewBehaviourScript : MonoBehaviour 
+{
+	int myInt = 5;
+
+	void Start()
+	{
+		myInt = MultiplyByTwo(myInt);
+		Debug.Log (myInt);
+	}
+
+	int MultiplyByTwo(int number)
+	{
+		int ret;
+
+		ret = number * 2;
+	
+		return ret;
+	}
+
+}
