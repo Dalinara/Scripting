ece01f55018cebd9ab38f9ad763fee73863cd922
unknown <10571815@CS514-11.ad.uvu.edu>
2015-09-21 09:10:12 -0600

    If Statements

---
+++ Unity1/Assets/IfStatement1.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IfStatement1 : MonoBehaviour 
+{
+	public float coffeeTemp = 85f;
+	public float hotLimit = 70f;
+	public float coldLimit = 40f;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown (KeyCode.Space))
+			TemperatureTest ();
+		
+		coffeeTemp -= Time.deltaTime * 5f;
+	}
+	
+	void TemperatureTest()
+	{
+		if (coffeeTemp > hotLimit) 
+		{
+			print ("Coffee is too hot");
+		} 
+		else if (coffeeTemp < coldLimit) 
+		{
+			print ("Coffee is too cold");
+		}
+		else 
+		{
+			print ("Coffee is just right");
+		}
+	}
+	
+	
+}
