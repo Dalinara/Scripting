93f3a23840f00ba2e33960283eba179ee92837c0
unknown <10571815@CS514-11.ad.uvu.edu>
2015-10-19 08:49:55 -0600

    Scope and Access Modifiers

---
+++ Unity1/Assets/Script/Scope/Ash.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ash : MonoBehaviour 
+{
+	public string pokedex;
+
+	private int pokeball;
+	private int potion;
+
+	public int pokemon = 2;
+
+	public StarterPokemon bag;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Space)) 
+		{
+			Pokemon();
+		}	
+		
+	}
+
+	void Pokemon ()
+	{
+		bag = new StarterPokemon();
+		bag.Pokeball(pokemon);
+		pokemon--;
+	}
+
+	//Pokeball
+
+}
---
+++ Unity1/Assets/Script/Scope/StarterPokemon.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarterPokemon : MonoBehaviour 
+{
+
+	// Use this for initialization
+	public void Pokeball (int pokemon = 2)
+	{
+		if (pokemon > 1) 
+		{
+			Debug.Log ("Congratulations! You have gotten a Charmander!");
+			pokemon--;
+		} 
+		else if (pokemon > 0) 
+		{
+			print("Congratulations! You have gotten a Pikachu!");
+			pokemon--;
+		}
+		else
+		{
+			print ("Sorry, there are no more pokemon.");
+		}
+	}
+}
