ab256de3a2d09f7e573ff3e68b9df3a33f989915
unknown <10571815@CS514-11.ad.uvu.edu>
2015-11-18 09:47:22 -0700

    Duck Hunt Sprites and Events

---
+++ Unity1/Assets/Script/Array.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Array : MonoBehaviour 
+{
+	int[] myIntArray = {13, 27, 39};
+
+	// Use this for initialization
+	void Start () 
+	{
+		myIntArray [1] = 14;
+	}
+
+}
