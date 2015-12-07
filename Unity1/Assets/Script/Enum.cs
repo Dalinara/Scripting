a34e61ffa3088ef837b4ac8dc523afe5d3ee5e02
unknown <10571815@CS514-11.ad.uvu.edu>
2015-12-07 09:07:31 -0700

    Enumerations

---
+++ Unity1/Assets/Script/Enum.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enum : MonoBehaviour 
+{
+	enum Direction {North, East, South, West}
+
+	// Use this for initialization
+	void Start () 
+	{
+		Direction myDirection;
+
+		myDirection = Direction.North;
+
+		myDirection = ReverseDirection (myDirection);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	Direction ReverseDirection(Direction dir)
+	{
+		if (dir == Direction.North)
+			dir = Direction.South;
+
+		return dir;
+	}
+
+}
