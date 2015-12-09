31d1713718438b39b0ad1989de6821bb4aa299d9
unknown <10571815@CS514-11.ad.uvu.edu>
2015-12-09 09:31:10 -0700

    Switch Statements

---
+++ Unity1/Assets/Script/Switch.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Switch : MonoBehaviour 
+{
+	public int intelligence = 2;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Greet ();
+	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//	
+//	}
+
+	void Greet()
+	{
+		switch(intelligence)
+		{
+		case 2:
+			print ("Hello!");
+			break;
+		case 1:
+			print ("blargh!");
+			break;
+		default:
+			print ("Incorrect incorrect level.");
+			break;
+		}
+
+
+	}
+
+
+
+}
